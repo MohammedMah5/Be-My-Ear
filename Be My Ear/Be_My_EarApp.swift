67bffc9a09aefbb1c4b5855b6d6d0d1022ff98c4
Mohammed <mohmedahmeduae1@gmail.com>
2023-08-28 19:23:27 +0400

    Initial Commit

---
+++ Be My Ear/Be_My_EarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_My_EarApp.swift
+//  Be My Ear
+//
+//  Created by Mohmed Ahmed on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Be_My_EarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
