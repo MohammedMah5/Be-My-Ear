d5a0641e4005e11f5af1883f6933b772cc5d4bc4
Mohammed <mohmedahmeduae1@gmail.com>
2023-09-09 11:58:18 +0400

    9-9

--- Be My Ear/ContentView.swift
+++ Be My Ear/ContentView.swift
@@ -4,18 +4,93 @@
 //
 //  Created by Mohmed Ahmed on 28/08/2023.
 //
-
 import SwiftUI
-
 struct ContentView: View {
+    @State private var FirstLanguageOptions = ["English"]
+    @State private var FirstLanguageselectedOption = 0
+    @State private var SecondLanguageSelectedOption = 0
+    @State private var SecondLanguageOptions = ["International SL" , "France SL"]
+    @State private var LText:String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                VStack{
+                    
+                    HStack {
+                        Picker("Select an option", selection: $FirstLanguageselectedOption) {
+                            ForEach(0..<FirstLanguageOptions.count, id: \.self) { index in
+                                Text(FirstLanguageOptions[index])
+                                    
+                            }
+                            
+                        }.pickerStyle(MenuPickerStyle())
+                            .foregroundColor(.white)
+                            .tint(Color.black)
+                        
+                        
+                        
+                        Image(systemName: "arrow.left.arrow.right")
+                            .padding()
+                        Picker("Select an option", selection: $SecondLanguageSelectedOption) {
+                            ForEach(0..<SecondLanguageOptions.count, id: \.self) { index in
+                                Text(SecondLanguageOptions[index])
+                                    
+                            }
+                            
+                        }.pickerStyle(MenuPickerStyle())
+                            .foregroundColor(.white)
+                        .padding()
+                        .tint(.black)
+                    }
+                    
+//                    .background(Color.teal)
+                    
+                        .cornerRadius(20)
+                    
+                    Spacer()
+                    
+                    ZStack(alignment: .topLeading){
+                        TextEditor(text:$LText)
+                            .padding(5)
+                            
+                        
+                    }
+                    .font(.body)
+                    .background(Color.teal)
+                    .cornerRadius(8)
+                    .frame(height:100)
+                    Spacer()
+                    HStack{
+                        Button("Split") {
+                            var txt_array = Array(LText)
+                            print(txt_array[2])
+                        }
+                    }
+                    ZStack(){
+                      
+                    }
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    
+                    
+                }
+                
+                .frame(maxWidth: .infinity,maxHeight:.infinity)
+                .background(.white)
+                .tabItem {
+                    Label("Translator" , systemImage: "character.phonetic")
+                    
+                    
+                }
+            }
+            .navigationTitle("Translator")
+                .padding()
+                
+            
         }
-        .padding()
+        
+        
     }
 }
 
